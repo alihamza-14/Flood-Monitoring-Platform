1311a34933cf8d262a0ebe504bff82da4e888633
Ali Hamza <ali946484@gmail.com>
2024-07-04 15:27:32 +0500

    admin boundaries and animation

--- flood_monitoring_platform/mainpage/urls.py
+++ flood_monitoring_platform/mainpage/urls.py
@@ -4,6 +4,7 @@ from .views import get_mvt
 
 urlpatterns = [
     path('get_mvt/<str:layer>/<int:z>/<int:x>/<int:y>/', views.get_mvt, name='get_mvt'),
+    path('get_admin_mvt/<str:layer>/<int:z>/<int:x>/<int:y>/', views.get_admin_mvt, name='get_admin_mvt'),
     # path('flood_extent/',views.flood_extent, name='flood_extent'),
     path('',views.map_view,name='map_view'),
 ]
