18d0b036b0e3627bac20234a1d13e7e51897bc10
romaisak221 <162549696+romaisak221@users.noreply.github.com>
2024-07-04 10:22:35 +0500

    Update models.py

--- flood_monitoring_platform/mainpage/models.py
+++ flood_monitoring_platform/mainpage/models.py
@@ -3,7 +3,6 @@ from django.contrib.gis.db import models
 # Create your models here.
 
 class FloodExtent(models.Model):
-    name = models.CharField(max_length=100)
     geom = models.PolygonField()
 
     def __str__(self):
